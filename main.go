b1790846353657120ad432418257139105d40744
Gilgamesh <32211128+Dontmindmes@users.noreply.github.com>
2018-10-06 13:15:30 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var weight float64
+var height float64
+var maxsum float64
+
+func calculate() {
+	maxsum = (weight / height) / height
+	fmt.Printf("Your BMI is %.2f kg/m2", math.Ceil(maxsum*100)/100) //math.Ceil(x*100)/100
+}
+
+func main() {
+
+	fmt.Println("Please enter all data in imperial foramt: kg/meters")
+
+	fmt.Printf("Enter your weight (kilograms): ")
+	fmt.Scan(&weight)
+
+	fmt.Printf("Enter your height (meters): ")
+	fmt.Scan(&height)
+
+	calculate()
+
+}
